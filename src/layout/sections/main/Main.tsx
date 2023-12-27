d85315202db2d8123e7cd68f4299185a7ab78ada
Nikita Andreevich <xniki4x@gmail.com>
2023-12-27 18:00:57 +0300

    little changes at header mobile menu and main sections

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,11 +4,12 @@ import photo from '../../../assets/images/me.jpg'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {Theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Nikita Bogdanovskiy</span></Name>
@@ -31,26 +32,24 @@ const StyledMain = styled.section`
   display: flex;
 `
 
-const Photo = styled.img`
-  width: 350px;
-  height: 430px;
-  object-fit: cover;
-`
-
 const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
+  //font-size: 27px;
+  //font-weight: 400;
+  ${font({weight: 400, Fmax: 27, Fmin:20})}
+
 `
 const Name = styled.h2`
-  font-family: 'Josefin Sans',sans-serif;
-  font-size: 50px;
-  font-weight: 700;
+  ${font({family: "'Josefin Sans',sans-serif", weight: 700, Fmax: 50, Fmin:36})}
+  //font-family: 'Josefin Sans',sans-serif;
+  //font-size: 50px;
+  //font-weight: 700;
   letter-spacing: 2.5px;
   margin: 10px 0;
   
   span{
     position: relative;
     z-index: 1;
+    //white-space: nowrap;
     
     &::after {
       content: "";
@@ -66,14 +65,19 @@ const Name = styled.h2`
     }
   }
   
+  @media ${Theme.media.mobile} {
+    margin: 15px 0 22px;
+  }
 `
 const SmallText = styled.span`
   font-size: 14px;
   font-weight: 400;
+
 `
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 1;
+  margin-top: 65px;
   
   &::before {
     content: "";
@@ -85,5 +89,23 @@ const PhotoWrapper = styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+    @media ${Theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -17px;
+      left: 20px;
+    }
+  }
+`
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+  margin-right: 20px;
+  
+  @media ${Theme.media.mobile} {
+    width: 310px;
+    height: 380px;
   }
 `
